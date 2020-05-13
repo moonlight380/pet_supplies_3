7f082e8fbaea43dd757ff30b8409f6505ec2d202
sw00010 <sw00010@naver.com>
2020-05-13 18:01:05 +0900

    cc

---
+++ src/main/java/com/pet/p1/cart/CartVO.java
@@ -0,0 +1,34 @@
+package com.pet.p1.cart;
+
+public class CartVO {
+	private String id;
+	private long cnum;
+	private long productNum;
+	private long cAmount;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public long getCnum() {
+		return cnum;
+	}
+	public void setCnum(long cnum) {
+		this.cnum = cnum;
+	}
+	public long getProductNum() {
+		return productNum;
+	}
+	public void setProductNum(long productNum) {
+		this.productNum = productNum;
+	}
+	public long getcAmount() {
+		return cAmount;
+	}
+	public void setcAmount(long cAmount) {
+		this.cAmount = cAmount;
+	}
+	
+	
+}
